168ab97c0af0218ed3dcbd7eb69b1564f28c273a
Dubi <marie@debian-BULLSEYE-live-builder-AMD64>
2022-02-01 14:12:00 +0100

    a new graph email to Agnieszka

--- graph_a.R
+++ graph_a.R
@@ -1,14 +1,17 @@
 library(igraph)
 library(tidyverse)
-set.seed(13)#Change number here to re-randomize the shape of your graph
+#col = c("yellow","lightsalmon", "lavenderblush" , "darkolivegreen2" , "lightblue")
+col = c("yellow","lightsalmon", "lavenderblush","darkolivegreen2" , "grey")
+set.seed(14)#Change number here to re-randomize the shape of your graph
 origins = "https://handrit.is/en/manuscript/view/AM02-0191|https://handrit.is/en/manuscript/view/AM04-0051|https://handrit.is/en/manuscript/view/AM04-0049|https://handrit.is/en/manuscript/view/AM04-0045|https://handrit.is/en/manuscript/view/Rask115|https://handrit.is/en/manuscript/view/AM04-0046|https://handrit.is/en/manuscript/view/AM08-030|https://handrit.is/en/manuscript/view/AM02-0201|https://handrit.is/en/manuscript/view/AM08-265|https://handrit.is/en/manuscript/view/AM02-0200|https://handrit.is/en/manuscript/view/AM04-0353|https://handrit.is/en/manuscript/view/AM02-300|https://handrit.is/en/manuscript/view/AM04-0899"
 colorise<-function(url)
-  ifelse(grepl(str_to_lower(origins),url,fixed = FALSE),"red",
-         ifelse(grepl("bibliography",url,fixed = TRUE),"pink",
-                ifelse(grepl("biography",url,fixed = TRUE),"green",
-                       ifelse(grepl("https://handrit.is/en/search/results/",url,fixed = TRUE),"white",
-                              ifelse(grepl("manuscript/view",url,fixed = TRUE),"orange",
-                  "grey")))))
+  #c("Results" , "http://...manuscript/view",  "http://...bibliography" , "http://...biography", "Other"), 
+  ifelse(grepl(str_to_lower(origins),url,fixed = FALSE),col[1],#the shelfmarks
+         ifelse(grepl("manuscript/view",url,fixed = TRUE),col[2],
+                ifelse(grepl("bibliography",url,fixed = TRUE),col[3],
+                       ifelse(grepl("biography",url,fixed = TRUE),col[4],
+                              ifelse(grepl("https://handrit.is/en/search/results/",url,fixed = TRUE),"white",#the central dot in white
+                  col[5])))))
 read_csv("handrit_swedish_aa_stopword.csv")%>%
   transmute(source=str_to_lower(source),
             target=str_to_lower(target),
@@ -23,7 +26,7 @@ bind_rows(transmute(df_edges,name=source,label=source_label),
   group_by(name)%>%
   summarise(actual_label=paste0(unique(label),collapse = " // "))%>%
   #change rbinom values to put more or less labels
-  mutate(label=ifelse(rbinom(n(),1,0.05),actual_label,""),color=colorise(name))->df_nodes
+  mutate(label=ifelse(rbinom(n(),1,0.4),actual_label,""),color=colorise(name))->df_nodes
 #read_csv("handrit_swedish_a_formated.csv")%>%
 
 #df_anonymous <- df_edges[3]
@@ -36,19 +39,22 @@ lo <- layout_nicely(g_frame)
 #dev.off()
 plot(g_frame,
      rescale=FALSE,
-     layout=1*norm_coords(lo, -1, 1, -1.2, 1.2),
+     layout=1*norm_coords(lo, -1, 1, -1, 1),
      vertex.size=5,#Change size of the bubbles here
      vertex.label.cex = 0.5,#Change label size here
      #vertex.label=df_anonymous, #uncomment to get things anonymised
      edge.width=0.3)
 #legend('topleft',legend=levels(sizeCut),pt.cex=scaled,col='black',pch=21, pt.bg='orange'))
 
-legend("topleft", 
-       legend = c("Results" , "http://...manuscript/view",  "http://...bibliography" , "http://...biography", "Other"), 
-       col = c("red","orange", "pink" , "green" , "grey"), 
-       pch = c(20,20,20,20,20), bty = "n",  pt.cex = 3.5, cex = 0.8 , 
-       text.col = "black", horiz = FALSE, inset = c(0, 0.1),
+legend("bottomleft", 
+       legend = c("Shelfmarks" , "http://...manuscript/view",  "http://...bibliography" , "http://...biography", "Other"), 
+       col = col, 
+       pch = c(20,20,20,20,20), bty = "y",  pt.cex =2.5, cex = 0.8 , #pt.cex = bubble size
+       text.col = "black", horiz = FALSE, inset = c(0.6, -0.1),
+       #pch = c(20,20,20,20,20), bty = "n", pt.cex =3.9, cex = 0.9 , 
+       #text.col = "black", horiz = FALSE, inset = c(0.3, 0.1),
        xpd=TRUE,
+       text.font = 56
        )
 #dev.off()
 #for graphviz later:
