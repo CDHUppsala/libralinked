381272c3930e723442ab1fd2a064006c9d3ea6be
Dubi <marie@debian-BULLSEYE-live-builder-AMD64>
2022-06-22 15:52:25 +0200

    colorify graphs

--- Manuscripta/scrap_orgs/graph.R
+++ Manuscripta/scrap_orgs/graph.R
@@ -1,8 +1,24 @@
 library(igraph)
 library(tidyverse)
 set.seed(12)#Change number here to re-randomize the shape of your graph
+origins = "other"
 
-read_csv("./data_html.csv")%>%
+greps = c(origins,"a \\d+","b \\d+","c \\d+","d \\d+","cc0 1.0 universal", "organisations")
+grepsleg = c(origins,"a ","b ","c ","d ","Licence", "organisations")
+col = c("grey","lightsalmon", "lavenderblush1","darkolivegreen2" , "cyan3", "white","yellow") #Colors of the nodes se http://www.stat.columbia.edu/~tzheng/files/Rcolor.pdf
+set.seed(14)#Change number here to re-randomize the shape of your graph
+#origins = "https://handrit.is/en/manuscript/view/AM02-0191|https://handrit.is/en/manuscript/view/AM04-0051|https://handrit.is/en/manuscript/view/AM04-0049|https://handrit.is/en/manuscript/view/AM04-0045|https://handrit.is/en/manuscript/view/Rask115|https://handrit.is/en/manuscript/view/AM04-0046|https://handrit.is/en/manuscript/view/AM08-030|https://handrit.is/en/manuscript/view/AM02-0201|https://handrit.is/en/manuscript/view/AM08-265|https://handrit.is/en/manuscript/view/AM02-0200|https://handrit.is/en/manuscript/view/AM04-0353|https://handrit.is/en/manuscript/view/AM02-300|https://handrit.is/en/manuscript/view/AM04-0899"
+colorise<-function(url)
+  #c("Results" , "http://...manuscript/view",  "http://...bibliography" , "http://...biography", "Other"), 
+  ifelse(grepl(str_to_lower(greps[1]),url,fixed = FALSE),col[1],#the shelfmarks
+         ifelse(grepl(greps[2],url,fixed = FALSE),col[2],
+                ifelse(grepl(greps[3],url,fixed = FALSE),col[3],
+                       ifelse(grepl(greps[4],url,fixed = FALSE),col[4],
+                              ifelse(grepl(greps[5],url,fixed = FALSE),col[5],
+                                ifelse(grepl("cc",url,fixed = TRUE),col[6],#the central dot in white
+                  col[7]))))))
+
+read_csv("./data.csv")%>%
   transmute(source=str_to_lower(source),
             target=str_to_lower(target),
             source_label=str_to_lower(label_s),
@@ -15,7 +31,7 @@ bind_rows(transmute(df_edges,name=source,label=source_label),
 ) %>%
   group_by(name)%>%
   summarise(label=paste0(unique(label),collapse = " // "))%>%
-  mutate(label=ifelse(rbinom(n(), 1,0.999),label,""))->df_nodes#This line keeps 99% of labels
+  mutate(label=ifelse(rbinom(n(), 1,0.999),label,""),color=colorise(label))->df_nodes#This line keeps 99% of labels
   #n() is the number of rows.
 #n_nodes<-nrow(df_nodes)
 #df_anonymous<-1:n_nodes
@@ -26,20 +42,23 @@ pdf(paper="USr","graph.pdf")
 lo <- igraph::layout_nicely(g_frame)
 #lo <- igraph::layout_in_circle(g_frame)
 plot(g_frame,
+     
      rescale=FALSE,
-     layout=0.6*norm_coords(lo, -1.8, 1.8, -1.4, 1.5),
-     vertex.size=1,
+     layout=0.65*norm_coords(lo, -1.8, 1.8, -1.4, 1.5),
+     vertex.size=3.5,#node sizes
      vertex.label.cex = 0.5,#Change label size here
+     vertex.frame.color="white",
      #vertex.label=df_anonymous, #uncomment to get things anonymised
      edge.width=0.5)
      #legend('topleft',legend=levels(sizeCut),pt.cex=scaled,col='black',pch=21, pt.bg='orange'))
 
-# legend('topright', box.lwd = 1, box.col = "black",
-#        cex = 0.8,
-#        c('Comment','Response','Author','Commentor'),
-#        col=c("black","grey","red","orange"), 
-#        
-#        pch = c(NA,NA,20,20),#(0,0,20,20)
-#        lty = c(1,1,0,0),#0,
-#        #bg =c(NA,NA,"green","blue"),
-#        bty = "y",  pt.cex =2.5 )
+legend("bottomleft", 
+       legend = grepsleg[2:7], 
+       col = col[2:7], 
+       pch = c(20,20,20,20,20), bty = "y",  pt.cex =2.0, cex = 0.4 , #pt.cex = bubble size
+       text.col = "black", horiz = FALSE, inset = c(0.6, -0.1),
+       #pch = c(20,20,20,20,20), bty = "n", pt.cex =3.9, cex = 0.9 , 
+       #text.col = "black", horiz = FALSE, inset = c(0.3, 0.1),
+       xpd=TRUE,
+       text.font = 56
+       )
